33fbd108d058235934f4330a278a010a110ee54d
Luciano Ditzel <48848920+lditzel94@users.noreply.github.com>
2024-08-25 16:06:55 -0300

    wip: graphql api implementation (#1)

---
+++ src/main/kotlin/dev/luciano/accelerator/domain/Domain.kt
@@ -0,0 +1,16 @@
+package dev.luciano.accelerator.domain
+
+import dev.luciano.accelerator.configuration.logger.CompanionLogger
+import java.util.UUID
+
+data class Domain(
+    val id: UUID,
+    val data: String,
+) {
+    companion object : CompanionLogger() {
+        operator fun invoke() =
+            Domain(id = UUID.randomUUID(), data = "")
+                .log { info("Domain object provided", it) }
+    }
+
+}
