7341b5cb388eac87189dbbef7eeebb8f166fad93
Luciano Ditzel <lucianoditzel94@gmail.com>
2024-10-04 20:28:56 -0300

    build: remove deprecated kotlin compile config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 
 plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
-    id("org.springframework.boot") version "3.3.2"
+    id("org.springframework.boot") version "3.3.3"
     id("io.spring.dependency-management") version "1.1.6"
 }
 
@@ -75,19 +75,13 @@ dependencies {
 
 kotlin {
     compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = JVM_21
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "21"
-    }
 }
 
 tasks.register<Exec>("composeDown") {
